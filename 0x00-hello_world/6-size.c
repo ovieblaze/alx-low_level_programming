7cfc0699b0db9c068de195da44a82105d14e3f4a
ovieblaze <obogareovie@gmail.com>
2022-03-10 10:15:31 -0800

    c lang learning

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/* Main - Prints quote
+*  Returns zero on success
+*/
+
+
+
+int main(void)
+{
+	int aninteger;
+	char acharacter;
+	long along;
+	long long alonglong;
+	float afloat;
+
+	printf("Size of a char: %lu byte(s)\n", sizeof(acharacter));
+	printf("Size of an int: %lu byte(s)\n", sizeof(aninteger));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(along));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(alonglong));
+	printf("Size of a float: %lu byte(s)\n", sizeof(afloat));
+
+	return (0);
+}
